88fee889697860ba72307ddb9839abef4c4cc77f
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-05-12 13:23:07 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,11 +3,11 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const formEl = document.querySelector('.form');
 
 const createsValueObject = () => {
-  return (optionsValue = {
+  return {
     delay: formEl.elements.delay.valueAsNumber,
     step: formEl.elements.step.valueAsNumber,
     amount: formEl.elements.amount.valueAsNumber,
-  });
+  };
 };
 
 const createPromises = (position, delay) => {
@@ -25,24 +25,27 @@ const createPromises = (position, delay) => {
 
 const promiseProcessing = ({ delay, step, amount }) => {
   for (let position = 1; position <= amount; position += 1) {
-    createPromises(position, delay)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      })
-      .finally(() => {
-        formEl.reset();
-      });
+    createPromises(position, delay).then(onSuccess).catch(onFailure).finally(onFinally);
     delay += step;
   }
 };
 
 function onCreatePromises(e) {
   e.preventDefault();
-  createsValueObject();
-  promiseProcessing(optionsValue);
+
+  promiseProcessing(createsValueObject());
 }
+
+const onSuccess = ({ position, delay }) => {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+};
+
+const onFailure = ({ position, delay }) => {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+};
+
+const onFinally = () => {
+  formEl.reset();
+};
 
 formEl.addEventListener('submit', onCreatePromises);
